1f7731baf7f74749f8902a54e76b790ac2927681
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2020-11-25 10:49:33 +0000

    several

--- index.js
+++ index.js
@@ -3,66 +3,66 @@ $('body').scrollspy({ target: '#main-nav' });
 
 //add smoth scrolling. slim cdn version doesnt work!
 $('#main-nav a').on('click', function (e) {
-    // check for a hash value
-    if (this.hash !== '') {
-        //prevent default behavior
-        e.preventDefault();
-        //store hash
-        const hash = this.hash;
-        //animate smooth scroll
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function () {
-            //hash to the URL after scroll
-            window.location.hash = hash;
-        });
-    }
+  // check for a hash value
+  if (this.hash !== '') {
+    //prevent default behavior
+    e.preventDefault();
+    //store hash
+    const hash = this.hash;
+    //animate smooth scroll
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      900,
+      function () {
+        //hash to the URL after scroll
+        window.location.hash = hash;
+      }
+    );
+  }
 });
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
 // Button back to top
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $("#back-to-top").fadeIn();
-        } else {
-            $("#back-to-top").fadeOut();
-        }
-    });
-// scroll body to 0px on click
-$("#back-to-top").click(function () {
-    $("body,html").animate(
-        {
-            scrollTop: 0,
-        },
-        900
-        );
-        return false;
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate(
+      {
+        scrollTop: 0,
+      },
+      900
+    );
+    return false;
+  });
 });
 
-//form 
-let onlyForm = document.querySelector('#form1')
-
+//form
+let onlyForm = document.querySelector('#form1');
 
 onlyForm.addEventListener('submit', function () {
-    // e.preventDefault();
-   setTimeout(function (){
-      swal({
-        title: "Recibimos tu mensaje",
-        text: `Te responderemos en breve`,
-        icon: "success",
-      }); 
-   }, 1500)
-    //sucess message
-    
-    
+  // e.preventDefault();
+  setTimeout(function () {
+    swal({
+      title: 'Recibimos tu mensaje',
+      text: `Te responderemos en breve`,
+      icon: 'success',
+    });
+  }, 1500);
+  //sucess message
 });
 
-// //form 
+// //form
 // let onlyForm = document.querySelector('#form1')
-
 
 // onlyForm.addEventListener('submit', function (e) {
 //     // e.preventDefault();console.log('submit');
@@ -83,4 +83,3 @@ onlyForm.addEventListener('submit', function () {
 //     emailInput.value = '';
 //     textInput.value = '';
 // });
-
